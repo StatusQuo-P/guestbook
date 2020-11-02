da138e20fee2b36db1d7538f9600e2345375b13e
philip <schuster.philip@mailbox.org>
2020-11-02 03:43:29 +0100

    Added rejectsEmptyMail() test and Updated constructor arguments for mail usage

--- src/test/java/guestbook/GuestbookEntryUnitTests.java
+++ src/test/java/guestbook/GuestbookEntryUnitTests.java
@@ -30,18 +30,24 @@ class GuestbookEntryUnitTests {
 	void rejectsEmptyName() {
 
 		assertThatExceptionOfType(IllegalArgumentException.class)//
-				.isThrownBy(() -> new GuestbookEntry("", "May the 4th be with you!"));
+				.isThrownBy(() -> new GuestbookEntry("", "too.much@tests.fail","May the 4th be with you!"));
+	}
+
+	@Test
+	void rejectsEmptyMail(){
+		assertThatExceptionOfType(IllegalArgumentException.class)//
+				.isThrownBy(() -> new GuestbookEntry("Bard Bard", "","I hate MOBAs!"));
 	}
 
 	@Test
 	void rejectsEmptyText() {
 
 		assertThatExceptionOfType(IllegalArgumentException.class)//
-				.isThrownBy(() -> new GuestbookEntry("Ollie", ""));
+				.isThrownBy(() -> new GuestbookEntry("Ollie", "more.ollie@web.de",""));
 	}
 
 	@Test
 	void setsCreationDate() {
-		assertThat(new GuestbookEntry("Ollie", "May the 4th be with you!").getDate()).isNotNull();
+		assertThat(new GuestbookEntry("Ollie", "more.ollie@web.de","May the 4th be with you!").getDate()).isNotNull();
 	}
 }
